3105f6faa0b723deef9039b1e1c2a8d4d70bd3bf
laoleic4 <56494512+bilibilimrlei@users.noreply.github.com>
2022-04-17 12:34:32 +0800

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    val kotlinVersion = "1.5.30"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
+
+    id("net.mamoe.mirai-console") version "2.9.0-M1"
+}
+
+group = "org.laolittle.plugin.joinorquit"
+version = "2.0.31"
+
+repositories {
+    maven("https://maven.aliyun.com/repository/public")
+    mavenCentral()
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
+    }
+}
